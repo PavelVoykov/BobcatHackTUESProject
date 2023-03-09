2ea5adb84aba5849f72c9cdd698d32c856a16196
PavelVoykov <pavelepavlov@abv.bg>
2023-03-09 23:03:01 +0200

    Java-SQL INSERT SELECT working

--- JavaSQLComm/src/Main.java
+++ JavaSQLComm/src/Main.java
@@ -7,12 +7,14 @@ public class Main {
 
         public static void main (String[]args){
             try {
-                Connection connection = DriverManager.getConnection("jdbc:mysql:sql7.freesqldatabase.com:3306/sql7604300", "sql7604300", "UfKbvY7Gpw");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com:3306/sql7604300", "sql7604300", "UfKbvY7Gpw");
                 Statement statement = connection.createStatement();
-                statement.executeUpdate("INSERT INTO Coordinates VALUES (100.50, 234.567);");
+                statement.executeUpdate("INSERT INTO Coordinates (coordinateX, coordinateY) VALUES ("+100.567+"," +234.567+");");
                 ResultSet resultSet = statement.executeQuery("SELECT * FROM Coordinates");
                 while (resultSet.next()) {
-                    System.out.println(resultSet.getString("coordinateX, coordinateY"));
+                    System.out.println(resultSet.getString("id"));
+                    System.out.println(resultSet.getString("coordinateX"));
+                    System.out.println(resultSet.getString("coordinateY"));
                 }
             }catch (Exception e){
             e.printStackTrace();
