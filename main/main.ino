e172a7311eab6611567e72178f33b38bfa0e3c45
EmilianaAP <ema.andr33va@gmail.com>
2023-03-10 12:12:30 +0200

    Button is ready :)

--- main/main.ino
+++ main/main.ino
@@ -1,40 +1,36 @@
-////////////Libraries////////////
-#include <Adafruit_GPS.h>
+////////////LIBRARY////////////
 #include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+///////////BUTTON///////////
+int BUTTON_PIN = 4;
+int buttonPressedCount = 0;
+bool isPressed = true;
+
+unsigned long previousMillis = 0;
+unsigned long lastPressed = 0;
 
 ////////////GPS//////////////
-Adafruit_GPS GPS(&Wire);
 
-/////////Bluetooth/////////
+/////////BLUETOOTH/////////
 SoftwareSerial hc06(2,3);
 
-///////////Button////////////
-int buttonPin = 3;
-int buttonState;
 
 void setup() {
-/////////Serial communication///////////
-  while(!Serial){}
+  ///////////SERIAL_COMMUNICATION////////////
   Serial.begin(9600);
 
-  Serial.println("Adafruit GPS library basic I2C test");
-  GPS.begin(0x10);
+  ///////////BUTTON_SETUP////////////
+  pinMode(BUTTON_PIN, INPUT);
+
+  ////////////////////////////////////
 
   hc06.begin(9600);
-
-  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   /*/////////CHECK FOR GPS COM//////////
-  if(Serial.available()){
-    char c = Serial.read();
-    GPS.write(c);
-  }
-  if(GPS.available()){
-    char c = GPS.read();
-    Serial.write(c);
-  }
+  
 
   /////////CHECK FOR BLUETOOTH COM/////////
   if (hc06.available()){
@@ -45,10 +41,39 @@ void loop() {
     hc06.write(Serial.read());
   }*/
 
-  buttonState = digitalRead(buttonPin);
-  /*if (buttonState == HIGH) {
-    Serial.write("Button is high");
-  } else {
-    Serial.write("Button is low");
-  }*/
+  //////////BUTTON//////////
+  unsigned long currentMillis = millis();
+
+  if(digitalRead(BUTTON_PIN) == HIGH){
+    if(isPressed == false){
+      isPressed = true;
+
+      unsigned long timeSincePressed = (unsigned long)(currentMillis - previousMillis);
+      //Uncomment to check how long since last time button was pressed
+      //Serial.println(age);
+
+      if(timeSincePressed <= 5000){
+        //Uncomment to check buttonPressedCount
+        //Serial.println(buttonPressedCount);
+        buttonPressedCount++;
+
+        previousMillis = currentMillis;
+
+        if(buttonPressedCount >= 3){
+          Serial.println("SOS");
+          buttonPressedCount = 0;
+        }
+      }else{
+        buttonPressedCount = 0;
+      }
+    }
+  }else{
+    isPressed = false;
+  }
+
+  if(buttonPressedCount == 0){
+    previousMillis = currentMillis;
+  }
+
+  delay(10);
 }
